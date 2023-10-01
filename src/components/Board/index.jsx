9c33554eb94c06de3e70f5f85915234de0287903
varun-kajarekar <kajrekarvarun@gmail.com>
2023-10-01 10:19:29 +0530

    Added board support to mobile

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -20,35 +20,72 @@ const Board = ()=>{
         canvas.height = window.innerHeight
         
         context.strokeStyle = "white"
-        context.lineWidth=5
+        context.lineWidth=10
+
+
+
+        // For PC
 
         const handleMouseDown = (e) => {
             shouldDraw.current = true
             context.beginPath()
-            context.moveTo(e.clientX, e.clientY)
+            let x = e.clientX
+            let y = e.clientY
+            context.moveTo(x,y)
         }
-
         const handleMouseMove = (e) => {
             if (!shouldDraw.current) return
-            context.lineTo(e.clientX, e.clientY)
+
+            let x = e.clientX
+            let y = e.clientY
+
+            context.lineTo(x,y)
             context.stroke()
         }
-
         const handleMouseUp = (e) => {
             shouldDraw.current = false
 
         }
-
         canvas.addEventListener('mousedown', handleMouseDown)
         canvas.addEventListener('mousemove', handleMouseMove)
         canvas.addEventListener('mouseup', handleMouseUp)
 
 
+
+
+        // Mobile 
+        
+        const Touchstart = (e) => {
+            shouldDraw.current = true
+            context.beginPath()
+            context.moveTo(e.touches[0].clientX,e.touches[0].clientY)
+        }
+
+        const Touchmove = (e) => {
+            if (!shouldDraw.current) return
+
+            context.lineTo(e.touches[0].clientX,e.touches[0].clientY)
+            context.stroke()
+        }
+
+        const Touchend = (e) => {
+            shouldDraw.current = false
+
+        }
+
+        canvas.addEventListener('touchstart', Touchstart)
+        canvas.addEventListener('touchmove', Touchmove)
+        canvas.addEventListener('touchend', Touchend)
+
+
         return () => {
+            canvas.removeEventListener('touchstart', Touchstart)
+            canvas.removeEventListener('touchmove', Touchmove)
+            canvas.removeEventListener('touchend', Touchend)
+
             canvas.removeEventListener('mousedown', handleMouseDown)
             canvas.removeEventListener('mousemove', handleMouseMove)
             canvas.removeEventListener('mouseup', handleMouseUp)
-
         }
 
 
